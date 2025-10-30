21d4d3ac40aff8465c8b320904e3a81f736a2eed
Bb K <bbk09274@gmail.com>
2025-10-30 08:44:35 +0000

    show the username here as well (_for element div_)

--- src/app/(main)/profile/[id]/page.tsx
+++ src/app/(main)/profile/[id]/page.tsx
@@ -106,6 +106,7 @@ export default function WorkerProfilePage({params: paramsPromise}: {params: Prom
                 />
               </div>
               <h1 className="text-3xl font-bold font-headline">{worker.name}</h1>
+              <p className="text-muted-foreground mb-2">@{worker.username}</p>
               <p className="text-lg text-primary font-semibold">{worker.profession}</p>
               <div className="flex items-center gap-2 mt-2">
                 <StarRating rating={averageRating} />
